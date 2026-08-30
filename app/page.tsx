d3f0a70cf162f030b17028f36ee60e9dee1e3073
Łukasz Nowakowski <lukasz@lnow.pl>
2026-08-30 10:27:20 +0200

    Add i18n for home page.
    Work on #4

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import { useTranslations } from "next-intl";
+
 import PageContainerComponent from "@/components/pages/page-container";
 
 import Summaries from "@/components/pages/page/summaries";
 import Widgets from "@/components/pages/page/widgets";
 
 export default function Page() {
+  const t = useTranslations("App/Page");
   return (
-    <PageContainerComponent title="Dashboard" subtitle="Welcome back, Alex. Here's your financial overview.">
+    <PageContainerComponent title={t('title')} subtitle={t('subtitle', { name: 'Alex' })}>
       <Summaries />
       <Widgets />
     </PageContainerComponent>
--- components/pages/layout/header.test.tsx
+++ components/pages/layout/header.test.tsx
@@ -5,7 +5,7 @@ import HeaderComponent from './header';
 
 describe('HeaderComponent', () => {
     test('renders correctly', async () => {
-        const screen = await render(<HeaderComponent setSidebarOpen={() => { }} />);
+        const screen = await render(<HeaderComponent setSidebarOpen={() => { }} sidebarOpen={true} />);
         const element = await screen.locator.getByLabelText('Toggle menu');
         expect(element.elements().length).toBeGreaterThan(0);
     });
